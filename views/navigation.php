82620c151a69e8ac5aa907090a4563aa7f3f32a9
Derek Myers <arcticpro@gmail.com>
2012-07-11 00:07:03 -0700

    Removing navigation peristent as you can just set active

--- views/navigation.php
+++ views/navigation.php
@@ -2,7 +2,7 @@
 <?php $pos = 0; ?>
 <ul class="nav">
 	<?php foreach ($links as $link) : ?>
-		<li class="<?php if ($link['active']) : ?>active<?php endif; ?><?php if (isset($link['persistent'])) : ?> persistent<?php endif; ?><?php if ($pos == 0) : ?> first<?php endif; ?><?php if (($pos + 1) != $total) : ?> last<?php endif; ?>">
+		<li class="<?php if ($link['active']) : ?>active<?php endif; ?><?php if ($pos == 0) : ?> first<?php endif; ?><?php if (($pos + 1) != $total) : ?> last<?php endif; ?>">
 			<?php echo Html::anchor($link['url'], $link['title'], array('class' => strtolower($link['title']).'Link')) ?>
 		</li>
 		<?php $pos++; ?>

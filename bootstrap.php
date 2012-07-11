d273958247b8aae6a859ed3567aff306b5b60b63
Derek Myers <arcticpro@gmail.com>
2012-07-10 22:43:03 -0700

    Adding twig extension

--- bootstrap.php
+++ bootstrap.php
@@ -3,11 +3,12 @@
 Autoloader::add_core_namespace('Bootstrap');
 
 Autoloader::add_classes(array(
-	'Bootstrap\\Bootstrap'   => __DIR__.'/classes/bootstrap.php',
-	'Bootstrap\\Alerts'      => __DIR__.'/classes/alerts.php',
-	'Bootstrap\\Breadcrumbs' => __DIR__.'/classes/breadcrumbs.php',
-	'Bootstrap\\Form'        => __DIR__.'/classes/form.php',
-	'Bootstrap\\Pagination'  => __DIR__.'/classes/pagination.php',
+	'Bootstrap\\Bootstrap'             => __DIR__.'/classes/bootstrap.php',
+	'Bootstrap\\Alerts'                => __DIR__.'/classes/alerts.php',
+	'Bootstrap\\Breadcrumbs'           => __DIR__.'/classes/breadcrumbs.php',
+	'Bootstrap\\Form'                  => __DIR__.'/classes/form.php',
+	'Bootstrap\\Pagination'            => __DIR__.'/classes/pagination.php',
+	'Bootstrap\\Bootstrap_Addons_Twig' => __DIR__.'/classes/bootstrap/addons/twig.php',
 ));
 
 // Initialize the bootstrap.
---
+++ classes/bootstrap/addons/twig.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Twig Exetnsion Class
+ *
+ * @package		Bootstrap
+ * @category	Package
+ * @author		Derek Myers
+ * @link		https://github.com/dmyers/fuel-bootstrap
+ */
+ 
+namespace Bootstrap;
+
+class Bootstrap_Addons_Twig extends \Twig_Extension
+{
+	/**
+	 * Gets the name of the extension.
+	 *
+	 * @return  string
+	 */
+	public function getName()
+	{
+		return 'bootstrap';
+	}
+
+	/**
+	 * Sets up all of the functions this extension makes available.
+	 *
+	 * @return  array
+	 */
+	public function getFunctions()
+	{
+		return array(
+			'render_alerts'      => new \Twig_Function_Function('Alerts::render'),
+			'render_breadcrumbs' => new \Twig_Function_Function('Breadcrumbs::render'),
+		);
+	}
+}

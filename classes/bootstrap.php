ac035f5e5c61051acf25d9a92c4ccc52db6eb576
Derek Myers <arcticpro@gmail.com>
2012-07-11 21:47:06 -0700

    Adding abillity to disable autoloading of bootstrap assets and to change path

--- classes/bootstrap.php
+++ classes/bootstrap.php
@@ -14,17 +14,45 @@ namespace Bootstrap;
 class Bootstrap
 {
 	/**
+	 * Initialize by loading config
+	 */
+	public static function _init()
+	{
+		\Config::load('bootstrap', true);
+	}
+
+	/**
 	 * Initialize bootstrap
 	 */
 	public static function init()
 	{
+		$autoload = \Config::get('bootstrap.path', false);
+
+		if (!$autoload) {
+			return;
+		}
+
+		$path = \Config::get('bootstrap.path', '');
+		$use_min = \Config::get('bootstrap.use_min', true);
+
+		$css_file = $path.'bootstrap';
+		$js_file = $path.'bootstrap';
+
+		if ($use_min) {
+			$css_file .= '.min';
+			$js_file .= '.min';
+		}
+
+		$css_file .= '.css';
+		$js_file .= '.js';
+
 		// See if Casset package is used.
 		if (\Package::loaded('casset')) {
-			\Casset::css('bootstrap.css');
-			\Casset::js('bootstrap.min.js');
+			\Casset::css($css_file, !$use_min);
+			\Casset::js($js_file, !$use_min);
 		} else {
-			\Asset::css('bootstrap.min.css');
-			\Asset::js('bootstrap.min.js');
+			\Asset::css($css_file);
+			\Asset::js($js_file);
 		}
 	}
 
---
+++ config/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * NOTICE:
+ *
+ * If you need to make modifications to the default configuration, copy
+ * this file to your app/config folder, and make them in there.
+ *
+ * This will allow you to upgrade fuel without losing your custom config.
+ */
+
+return array(
+	/**
+	 * use_min - Whether to autoload bootstrap assets.
+	 */
+	'autoload' => true,
+
+	/**
+	 * path - The path where bootstrap files are.
+	 *
+	 * Leave empty for main assets folder.
+	 */
+	'path' => '',
+
+	/**
+	 * use_min - Whether to load minified version.
+	 */
+	'use_min' => true,
+);

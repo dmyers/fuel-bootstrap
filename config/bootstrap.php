5445e0248a6bbbcb1cc74175c6ed601c17351c22
Derek Myers <arcticpro@gmail.com>
2012-12-30 19:34:16 -0600

    Changed docblocks in configs

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,7 +6,7 @@
  * If you need to make modifications to the default configuration, copy
  * this file to your app/config folder, and make them in there.
  *
- * This will allow you to upgrade fuel without losing your custom config.
+ * This will allow you to upgrade bootstrap without losing your custom config.
  */
 
 return array(

16a97428c466b94182da8860d2d393d10fa0f2c0
Derek Myers <arcticpro@gmail.com>
2012-12-30 18:28:38 -0600

    Fixed last class on li in navigation view

--- views/navigation.php
+++ views/navigation.php
@@ -7,7 +7,7 @@
 	</li>
 	<?php endif; ?>
 	<?php foreach ($links as $link) : ?>
-		<li class="<?php if ($link['active']) : ?>active<?php endif; ?><?php if ($pos == 0) : ?> first<?php endif; ?><?php if (($pos + 1) != $total) : ?> last<?php endif; ?>">
+		<li class="<?php if ($link['active']) : ?>active<?php endif; ?><?php if ($pos == 0) : ?> first<?php endif; ?><?php if (($pos + 1) == $total) : ?> last<?php endif; ?>">
 			<?php echo Html::anchor($link['url'], $link['title'], array('class' => $link['class'])) ?>
 		</li>
 		<?php $pos++; ?>
